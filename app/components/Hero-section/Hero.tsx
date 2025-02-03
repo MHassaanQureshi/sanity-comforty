7874b88ff1be4503899fa081742822ad78d9255b
MHassaanQureshi <156497587+MHassaanQureshi@users.noreply.github.com>
2025-02-03 14:11:09 +0500

    Update Hero.tsx

--- app/components/Hero-section/Hero.tsx
+++ app/components/Hero-section/Hero.tsx
@@ -4,8 +4,8 @@ export default function Hero(){
     return(
         <div className="bg-[#F0F2F3] w-full flex flex-col md:flex-row md:w-[80%] md:p-4 md:h-[30rem] md:items-center ">
             <div className="flex flex-col p-2  w-[70%] md:ml-20"> 
-                <p className="text-[#272343] ">WELCOME TO CHAIRY</p>
-                <p className="text-[#272343] font-bold text-4xl p-2">Best Furniture Collection for your interior.</p>
+                <p className="text-[#272343] ">WELCOME TO COMFORTY</p>
+                <p className="text-[#272343] font-bold text-4xl p-2">Where Style Meets Comfort.</p>
                 <button className="bg-[#029FAE] rounded-lg flex flex-row py-2 text-white gap-3 w-[60%] items-center justify-center"><Link href="/Products">Shop Now </Link><Image src="/images/right-arrow.png" alt="unable to load" width={20} height={20} className="hover:scale-105 transition-transform duration-500" /></button>
             </div>
             <div className="w-full  flex justify-center items-center">
